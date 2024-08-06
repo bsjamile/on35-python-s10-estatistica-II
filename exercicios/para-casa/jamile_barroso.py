e913873d96918ad97179efcc113f4d793821c96b
Jamile <jamilebarroso0@gmail.com>
2024-08-06 13:41:00 -0300

    atv casa

---
+++ exercicios/para-casa/jamile_barroso.py
@@ -0,0 +1,18 @@
+# **Exercício: Teste z para Altura Média da Turma**
+
+# Cenário:
+
+# Queremos avaliar se a altura média da turma on35 é significativamente diferente da altura média nacional da população feminina brasileira, que é de 161 cm. 
+
+# Objetivo:
+
+#     Verificar se a altura média da turma é significativamente diferente de 161 cm.
+
+# Dados da Amostra: até o momento (20 Meninas - até Rafa Roden):
+#     Altura da turma (em cm): [165, 157, 163, 170, 167, 161, 160, 170, 157, 160, 157, 168, 162, 170, 167, 165, 166, 158, 174, 169]
+
+# Perguntas:
+
+# Calcule a estatística *z* e o valor *p* para verificar se a altura média da turma é significativamente diferente de 161 cm.
+
+# Interprete os resultados: Preciso saber se rejeitou ou não a hipótese nula.
